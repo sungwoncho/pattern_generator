127e8b9e7037de4b3d4c61e36c66e9774849ca33
Sung Won Cho <mikeswcho@gmail.com>
2015-06-10 17:17:09 +1000

    Copy service spec template to appropriate directory

--- lib/generators/service/service_generator.rb
+++ lib/generators/service/service_generator.rb
@@ -2,6 +2,10 @@ class ServiceGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
 
   def copy_service_file
-    copy_file 'service.rb', "#{file_name}_service.rb"
+    copy_file 'service.rb', "app/services/#{file_name}_service.rb"
+  end
+
+  def copy_service_spec_file
+    copy_file 'service_spec.rb', "spec/services/#{file_name}_service_spec.rb"
   end
 end
--- test/generators/service/service_generator_test.rb
+++ test/generators/service/service_generator_test.rb
@@ -3,18 +3,24 @@ require 'generators/service/service_generator'
 
 class ServiceGeneratorTest < Rails::Generators::TestCase
   tests ServiceGenerator
-  destination File.join(Rails.root, 'app/services')
 
-  setup :prepare_destination
+  destination File.join(Rails.root)
   teardown :cleanup
 
   test 'correct file is generated' do
     run_generator %w(find_match)
 
-    assert_file 'find_match_service.rb'
+    assert_file 'app/services/find_match_service.rb'
+  end
+
+  test 'correct spec file is generated' do
+    run_generator %w(find_match)
+
+    assert_file 'spec/services/find_match_service_spec.rb'
   end
 
   def cleanup
     FileUtils.rm_rf(File.join(Rails.root, 'app/services'))
+    FileUtils.rm_rf(File.join(Rails.root, 'spec/services'))
   end
 end
